1497fe294e7e203fe20e3b0288a1013dcf76465e
Fm1sk13w <f.m1skiew@gmail.com>
2025-09-09 20:50:53 +0200

    updated setup.py

--- poetry_project/setup.py
+++ poetry_project/setup.py
@@ -5,15 +5,18 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "requests",          # HTTP requests for scraping and APIs
-        "beautifulsoup4",    # HTML parsing
-        "pronouncing",       # CMU Pronouncing Dictionary
-        "pandas",            # Data handling and analysis
-        "matplotlib",        # Plotting
-        "seaborn",           # Statistical data visualization
-        "scipy",             # Statistical functions (pearsonr, etc.)
-        "tqdm",              # Progress bars
-        "pyarrow",           # Parquet file support for metrics
+        "requests",
+        "beautifulsoup4",
+        "pronouncing",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "statsmodels",
+        "scikit-learn",
+        "tqdm",
+        "pyarrow",
     ],
     python_requires=">=3.9",
 )
